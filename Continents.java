8c700cc850f56f5671614e9c3e644d28f5f125e9
Quinn Rodney <followthewhiterabit93@gmail.com>
2020-09-25 11:02:30 -0500

    Create Continents.java
    Prints out a continent and the largest city in that continent, based on the value of an integer.

---
+++ Continents.java
@@ -0,0 +1,32 @@
+//A java program that will print out a continetn and the largest city in the cc
+public class Continents {
+	public static void main(String[] args) {
+    int continent = 4;
+    switch (continent) {
+      case 1:
+      System.out.println("North America: Mexico City, Mexico");
+        break;
+      case 2:
+      System.out.println("South America: Sao Paulo, Brazil");
+        break;
+      case 3:
+      System.out.println("Europe: Moscow, Russia");
+        break;
+      case 4:
+      System.out.println("Africa: Lagos, Nigeria");
+        break;
+      case 5:
+      System.out.println("Asia: Shanghai, China");
+        break;
+      case 6:
+      System.out.println("Austrailia: Sydney, Australia");
+        break;
+      case 7:
+      System.out.println("Antartica: McMurdo Station, US");
+        break;
+      default:
+      System.out.println("Undefined continent!");
+        break;
+    }
+	}
+}
