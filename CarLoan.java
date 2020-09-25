97f680f0dc7271e4255fa86bffd472940406ee73
Quinn Rodney <followthewhiterabit93@gmail.com>
2020-09-25 08:54:47 -0500

    Create CarLoan.java
    A java program that will calculate the monthly car payment a user should expect to make after taking out a car loan. The program will include the following: car loan amount, interest rate of the loan, length of the loan, down payment.

---
+++ CarLoan.java
@@ -0,0 +1,23 @@
+/*CarLoan takes in a request car loan amount, then using the loan length combined with the estimated interest rate, and down payment return the montly payment amount?*/
+
+public class CarLoan {
+	public static void main(String[] args) {
+    int carLoan = 10000;
+    int loanLength = 3;
+    int interestRate = 5;
+    int downPayment = 2000;
+
+    if (loanLength <= 0 || interestRate <= 0) {
+      System.out.println("Error! You must take out a valid car loan.");
+    } else if (downPayment >= carLoan){
+      System.out.println("The car can be paid in full.");
+    } else {
+      int remainingBalance = carLoan - downPayment;
+      int months = loanLength * 12;
+      int monthlyBalance = remainingBalance / months;
+      int interest = (monthlyBalance * interestRate) / 100;
+      int monthlyPayment = monthlyBalance + interest;
+      System.out.println(monthlyPayment);
+    }
+	}
+}
