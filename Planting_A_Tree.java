7a18c9e4008837d0ab1dd3aaea98e38a2548b1e8
Quinn Rodney <followthewhiterabit93@gmail.com>
2020-09-25 08:35:14 -0500

    Create Planting_A_Tree.java
    Write a program that introduces yourself in a string, and prints a tree.

---
+++ Planting_A_Tree.java
@@ -0,0 +1,13 @@
+// Define your tree class in this file!
+public class Tree{ 
+  public static void main(String[] args) {
+    //write a string that introduces yourself and print a tree to the screen
+    System.out.println("Hey there, I'm Quinn and I'm learning to code in Java!");
+    System.out.println("I'm going to plant a tree today!");
+    System.out.println(" **** ");
+    System.out.println(" *** ");
+    System.out.println(" *** ");
+    System.out.println("  *  ");
+    System.out.println("  *  ");  
+  }
+}
